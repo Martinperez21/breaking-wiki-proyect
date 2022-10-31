b4be7661cd42f11385477e8a3a2e287fd60ceb5c
Herrera Franco <69646385+scott1ee@users.noreply.github.com>
2022-10-31 15:02:31 -0300

    muestro los datos traidos de la api
    Nota: Falta que se muestren varios a la vez y funcionalidad de paginacion

--- main.js
+++ main.js
@@ -2,14 +2,21 @@ const request = new XMLHttpRequest();
 request.open('GET', 'https://www.breakingbadapi.com/api/character/random', true);
 
 request.onload = function () {
-  if (request.status >= 200 && request.status < 400) {
-    const data = JSON.parse(this.response);
-    data.forEach((characters) => {
-      console.log(characters);
-    });
-  } else {
-    console.log('Ha ocurrido un error con código ' + request.status);
-  }
+  const data = JSON.parse(this.response);
+  data.forEach((characters) => {
+    // Muestro imagen
+    const image = document.getElementById("image");
+    image.src = characters.img;
+    // Muestro nombre
+    const name = document.getElementById("name");
+    name.textContent = characters.name;
+    // Muesto ocupacion
+    const occupation = document.getElementById("occupation");
+    occupation.textContent = characters.occupation[0];
+    // Muestro interprete
+    const portrayed = document.getElementById("portrayed");
+    portrayed.textContent = "Portrayed : " + characters.portrayed;
+  });
 }
 
 request.send();
